2397929c50decdc08ad73e28c05604fbf7687ba6
PraneeThongsamrit <62647929+PraneeThongsamrit@users.noreply.github.com>
2020-03-27 19:56:57 +0700

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,7 @@
-var Math = require("../js/Math.js");
+var Math = require("../js/math.js");
 var appRouter = function (app) {
   app.get('/', function (req, res) {
-    res.send('<h1>FumerAPI is now.</h1>')
+    res.send('<h1>PlalaMerAPI is now.</h1>')
   });
 
   // Bisection-Method
